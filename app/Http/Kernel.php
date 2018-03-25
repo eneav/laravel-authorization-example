1f1c790c86388bcbc47d113c0928ee3ec4c1daad
enea dhack <enea.so@live.com>
2018-03-25 11:56:36 -0500

    Enabled laravel-authorization middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,9 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        // laravel-authorization
+        'authenticated.can' => \Enea\Authorization\Middleware\PermissionAuthorizerMiddleware::class,
+        'authenticated.is' => \Enea\Authorization\Middleware\RoleAuthorizerMiddleware::class,
     ];
 }
